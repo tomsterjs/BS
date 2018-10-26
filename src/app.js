5222bad5e118ae524030465d6455bff7a4707372
dmytros <7ernya@gmail.com>
2018-10-26 10:05:40 -0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const logger = require('morgan');
+const searchRouter = require('./api/product/search');
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use('/product', searchRouter);
+
+// error handler
+app.use(function (err, req, res) {
+  res.status(500).send('Something is broken. Please try other API')
+});
+
+module.exports = app;
